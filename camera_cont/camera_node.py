7db51eb842d54b8e7db3b715e787b135fba09bf7
Filip Šijak <sijakf3@gmail.com>
2025-10-24 01:41:32 +0200

    v25.43.rc21

--- camera_cont/camera_node.py
+++ camera_cont/camera_node.py
@@ -1,41 +1,29 @@
-#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+import numpy as np
 import cv2
+from picamera2 import Picamera2
+from cv_bridge import CvBridge
 
-class CameraNode(Node):
+class CameraPublisher(Node):
     def __init__(self):
-        super().__init__('camera_node')
-        self.publisher_ = self.create_publisher(Image, 'camera/image_raw', 10)
+        super().__init__('camera_publisher')
+        self.publisher_ = self.create_publisher(Image, '/camera/image_raw', 10)
+        self.timer = self.create_timer(1/30, self.publish_frame)  # 30 FPS
+        self.picam2 = Picamera2()
+        self.picam2.start()
         self.bridge = CvBridge()
-        self.cap = cv2.VideoCapture(0)  # /dev/video0
 
-        if not self.cap.isOpened():
-            self.get_logger().error("Ne mogu otvoriti kameru")
-            exit(1)
-
-        timer_period = 0.1  # 10 Hz
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.get_logger().info("Camera node pokrenut")
-
-    def timer_callback(self):
-        ret, frame = self.cap.read()
-        if not ret:
-            self.get_logger().warning("Ne mogu pročitati frame")
-            return
-        msg = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+    def publish_frame(self):
+        frame = self.picam2.capture_array()
+        msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
         self.publisher_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = CameraNode()
-    try:
-        rclpy.spin(node)
-    except KeyboardInterrupt:
-        pass
-    node.cap.release()
+    node = CameraPublisher()
+    rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
