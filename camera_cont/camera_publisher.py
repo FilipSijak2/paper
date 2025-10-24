f2b9026b122aec8bc9c3e5616da62582fe14d81b
Filip Šijak <sijakf3@gmail.com>
2025-10-24 02:03:35 +0200

    v24.43.rc24

--- camera_cont/camera_publisher.py
+++ camera_cont/camera_publisher.py
@@ -7,16 +7,17 @@ from cv_bridge import CvBridge
 class CameraPublisher(Node):
     def __init__(self):
         super().__init__('camera_publisher')
-        self.pub = self.create_publisher(Image, 'camera/image_raw', 10)
+        self.publisher_ = self.create_publisher(Image, 'camera/image_raw', 10)
         self.bridge = CvBridge()
         self.cap = cv2.VideoCapture(0)  # /dev/video0
-        self.timer = self.create_timer(0.1, self.publish_frame)  # 10 Hz
 
-    def publish_frame(self):
+        self.timer = self.create_timer(1.0/30, self.timer_callback)  # 30 FPS
+
+    def timer_callback(self):
         ret, frame = self.cap.read()
         if ret:
             msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
-            self.pub.publish(msg)
+            self.publisher_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)
